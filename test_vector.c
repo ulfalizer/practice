3cc2dd072c88c81fc8014ff8fbb4b1df1e2db4f8
Ulf Magnusson <ulfalizer@gmail.com>
2014-11-15 07:02:14 +0100

    Fix broken vector test case.

--- test_vector.c
+++ test_vector.c
@@ -7,9 +7,9 @@ void test_vector() {
     vector_init(&vector);
     for (intptr_t i = 0; i < 512; ++i) {
         VERIFY(vector_len(&vector) == i);
-        vector_add(&vector, (void*)i);
-        for (intptr_t j = i; j <= i; ++j)
+        for (intptr_t j = 0; j < i; ++j)
             VERIFY((intptr_t)vector_get(&vector, j) == j);
+        vector_add(&vector, (void*)i);
     }
     vector_free(&vector);
 }

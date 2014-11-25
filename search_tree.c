c24e426ae21f136eb263d015bf6760099961c71c
Ulf Magnusson <ulfalizer@gmail.com>
2014-11-25 06:00:20 +0100

    Simplify search tree with traversal helper.
    The new form also makes GCC output a conditional move for selecting
    between the left and right pointers. Might improve performance vs. a
    conditional branch since the selection is unpredictable in general.

--- search_tree.c
+++ search_tree.c
@@ -10,52 +10,52 @@ void search_tree_free(Search_tree *tree) {
     tree_free(tree->root);
 }
 
-bool search_tree_set(Search_tree *tree, int key, int val, int *oldval) {
+// Returns the address of the pointer to the node holding 'key'. If 'key' does
+// not exist in the tree, returns the address of the (NULL) pointer that could
+// be updated to insert it.
+static Tree_node **get_node_ptr(Search_tree *tree, int key) {
     Tree_node **cur = &tree->root;
-    while (*cur)
-        if (key < (*cur)->key)
-            cur = &(*cur)->left;
-        else if (key > (*cur)->key)
-            cur = &(*cur)->right;
-        else {
-            if (oldval != NULL)
-                *oldval = (*cur)->val;
-            (*cur)->val = val;
-            return true;
-        }
-    *cur = create_node(key, val);
-    return false;
+
+    while (*cur && key != (*cur)->key)
+        cur = (key < (*cur)->key) ? &(*cur)->left : &(*cur)->right;
+    return cur;
+}
+
+bool search_tree_set(Search_tree *tree, int key, int val, int *oldval) {
+    Tree_node **node = get_node_ptr(tree, key);
+
+    if (*node == NULL) {
+        *node = create_node(key, val);
+        return false;
+    }
+
+    if (oldval != NULL)
+        *oldval = (*node)->val;
+    (*node)->val = val;
+    return true;
 }
 
 bool search_tree_get(Search_tree *tree, int key, int *val) {
-    Tree_node *cur = tree->root;
-    while (cur)
-        if (key < cur->key)
-            cur = cur->left;
-        else if (key > cur->key)
-            cur = cur->right;
-        else {
-            if (val != NULL)
-                *val = cur->val;
-            return true;
-        }
-    return false;
+    Tree_node *node = *get_node_ptr(tree, key);
+
+    if (node == NULL)
+        return false;
+
+    if (val != NULL)
+        *val = node->val;
+    return true;
 }
 
 bool search_tree_remove(Search_tree *tree, int key, int *val) {
-    Tree_node **cur = &tree->root;
-    while (*cur)
-        if (key < (*cur)->key)
-            cur = &(*cur)->left;
-        else if (key > (*cur)->key)
-            cur = &(*cur)->right;
-        else {
-            if (val != NULL)
-                *val = (*cur)->val;
-            tree_remove(cur);
-            return true;
-        }
-    return false;
+    Tree_node **node = get_node_ptr(tree, key);
+
+    if (*node == NULL)
+        return false;
+
+    if (val != NULL)
+        *val = (*node)->val;
+    tree_remove(node);
+    return true;
 }
 
 bool search_tree_valid(Search_tree *tree) {

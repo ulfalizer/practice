d1ea3055721fbce2438183ed160d7c6a8d41adf5
Ulf Magnusson <ulfalizer@gmail.com>
2014-12-09 23:30:09 +0100

    Small String simplification.

--- src/string.c
+++ src/string.c
@@ -20,18 +20,19 @@ _Static_assert(sizeof(unsigned long long) >= sizeof(size_t),
 
 void string_append(String *s, const char *format, ...) {
     va_list ap, ap_copy;
-    int addition_len;
+    size_t new_len;
 
     va_start(ap, format);
     // The first vsnprintf() will trash 'ap', so keep a copy in case we need to
     // repeat the operation.
     va_copy(ap_copy, ap);
 
-    addition_len = vsnprintf(s->buf + s->len, s->buf_len - s->len, format, ap);
-    if (s->len + addition_len >= s->buf_len) {
+    new_len = s->len +
+      vsnprintf(s->buf + s->len, s->buf_len - s->len, format, ap);
+    if (new_len >= s->buf_len) {
         // The resulting string won't fit in the buffer. Grow the buffer to the
         // next power of two that can accommodate it.
-        s->buf_len = ge_pow_2(s->len + addition_len + 1);
+        s->buf_len = ge_pow_2(new_len + 1);
         s->buf = erealloc(s->buf, s->buf_len, "string grow");
 
         // We should be good now. (Though maybe there's something extremely
@@ -40,7 +41,7 @@ void string_append(String *s, const char *format, ...) {
         // break this.)
         vsprintf(s->buf + s->len, format, ap_copy);
     }
-    s->len += addition_len;
+    s->len = new_len;
 
     va_end(ap_copy);
     va_end(ap);

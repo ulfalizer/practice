3c841ddf24a00080ab56dd0cafc5383c70574f53
Ulf Magnusson <ulfalizer@gmail.com>
2014-12-16 21:51:23 +0100

    eval() documentation nit.

--- include/eval.h
+++ include/eval.h
@@ -2,7 +2,7 @@
 // storing the result in 'res'. Returns false without modifying 'res' if the
 // expression is malformed. Catches all syntax errors.
 //
-// Supports integer arithmetic (to keep things simple), parentheses and the
+// Supports integer arithmetic (to keep things simple), parentheses, and the
 // operations +, -, *, /, and ** (exponentiation) with the usual precedence
 // rules (and the "SAFE" rules for exponentation from
 // http://macnauchtan.com/pub/precedence.html). Whitespace (isspace()) in

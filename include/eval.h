34b77b3af9513a25e0dae65d8b558a2bcb0845ca
Ulf Magnusson <ulfalizer@gmail.com>
2014-12-16 21:49:56 +0100

    Add more eval() documentation.

--- include/eval.h
+++ include/eval.h
@@ -1,7 +1,10 @@
-// Evaluates the arithmetic expression in 's' and stores the result in 'res'.
-// Returns false without modifying 'res' if the expression is malformed.
+// Evaluates the arithmetic expression in 's' using a top-down approach,
+// storing the result in 'res'. Returns false without modifying 'res' if the
+// expression is malformed. Catches all syntax errors.
 //
-// Supports parentheses and the operations +, -, *, /, and ** (exponentiation)
-// with the usual precedence rules (and the "SAFE" rules for exponentation from
-// http://macnauchtan.com/pub/precedence.html).
+// Supports integer arithmetic (to keep things simple), parentheses and the
+// operations +, -, *, /, and ** (exponentiation) with the usual precedence
+// rules (and the "SAFE" rules for exponentation from
+// http://macnauchtan.com/pub/precedence.html). Whitespace (isspace()) in
+// expressions is ignored.
 bool eval(const char *s, int *res);
